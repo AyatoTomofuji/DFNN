8fe3481045bf9c63cd7a6a4ad3fa1d6f18f9cd54
Ayato <seb01090@st.osakafu-u.ac.jp>
2024-07-22 17:38:06 +0900

    識別を，ucimlrepoなどのデータセットを用いて行う

--- shikibetsu.py
+++ shikibetsu.py
@@ -1,8 +1,10 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from sklearn.datasets import load_iris, load_breast_cancer, load_wine
+from sklearn.datasets import load_iris, load_breast_cancer, load_wine, load_digits
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import tree
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 import numpy as np
@@ -12,7 +14,8 @@ import matplotlib.pyplot as plt
 iris = load_iris()
 wine = load_wine()
 cancer = load_breast_cancer()
-set = cancer
+digits = load_digits()
+set = digits
 
 X = set.data
 y = set.target
@@ -21,14 +24,12 @@ y = set.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # データの標準化
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+
 n_features = X.shape[1]
 out = len(y)
 # ニューラルネットワークの定義
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, n_features, out):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(n_features, 128)
         self.fc2 = nn.Linear(128, out)
@@ -38,7 +39,7 @@ class Net(nn.Module):
         x = self.fc2(x)
         return x
 
-net = Net()
+net = Net(n_features, out)
 
 # 損失関数とオプティマイザ
 criterion = nn.CrossEntropyLoss()
@@ -64,7 +65,6 @@ with torch.no_grad():
     accuracy = (predicted.numpy() == y_test).sum() / y_test.size
     print(f'Test Accuracy: {accuracy:.4f}')
 
-print(predicted)
 # 訓練データ全体の予測結果を取得
 net.eval()
 with torch.no_grad():
@@ -72,8 +72,10 @@ with torch.no_grad():
     _, train_predicted = torch.max(train_outputs.data, 1)
     train_predicted = train_predicted.numpy()
 
-from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
+
+grader = tree.DecisionTreeClassifier()
+grader.fit(X_train, y_train)
+grade = grader.predict(X_test)
 
 # 決定木の訓練
 clf = RandomForestClassifier()
@@ -83,23 +85,17 @@ test_outputs = clf.predict(X_test)
 accuracy_tree = np.sum(test_outputs == y_test) / len(y_test)
 print(f"ランダムフォレストによる{accuracy_tree}")
 
-clf2 = DecisionTreeClassifier(max_depth=4)
-clf2.fit(X_train, y_train)
-test_outputs2 = clf2.predict(X_test)
-accuracy_tree2 = np.sum(test_outputs2 == y_test) / len(y_test)
-print(f"決定木の精度{accuracy_tree2}")
 
-clf3 = DecisionTreeClassifier(max_depth=4)
+dt = DecisionTreeClassifier(max_depth=4)
 
-clf3.fit(X_train, train_predicted)
-test_outputs3 = clf3.predict(X_test)
+dt.fit(X_train, train_predicted)
+test_outputs3 = dt.predict(X_test)
 accuracy_tree3 = np.sum(test_outputs3 == y_test) / len(y_test)
 print(f"ランダムフォレストから学習した決定木の精度{accuracy_tree3}")
 
 
-
-
+class_names = [str(name) for name in digits.target_names]
 # 決定木の可視化
 plt.figure(figsize=(20,10))
-tree.plot_tree(clf2, feature_names=set.feature_names, class_names=set.target_names, filled=True)
+tree.plot_tree(dt, feature_names=set.feature_names, class_names=class_names, filled=True)
 plt.show()
